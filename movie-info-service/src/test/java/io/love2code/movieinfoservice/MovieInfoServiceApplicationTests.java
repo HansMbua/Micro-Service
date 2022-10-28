f14be8d9b7668c2f4b7a038a16a966ec24cfb35d
hansmbua <hansmbua@yahoo.com>
2022-10-28 21:22:38 +0200

    MovieInfoServiceApplication.java added

---
+++ movie-info-service/src/test/java/io/love2code/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.love2code.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
